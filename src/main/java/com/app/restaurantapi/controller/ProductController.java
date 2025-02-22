77a00d530df3b974e09a3ac6bb831b1c698c1135
RefinedCandle49 <valdezvargaskevin@gmail.com>
2025-02-22 17:12:29 -0500

    ProductController.java created

---
+++ src/main/java/com/app/restaurantapi/controller/ProductController.java
@@ -0,0 +1,28 @@
+package com.app.restaurantapi.controller;
+
+import com.app.restaurantapi.entity.Product;
+import com.app.restaurantapi.service.ProductService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@CrossOrigin
+@AllArgsConstructor
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+    private ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping("/all")
+    public List<Product> findAllActiveProducts() {
+        return productService.findAllActiveProducts();
+    }
+}
