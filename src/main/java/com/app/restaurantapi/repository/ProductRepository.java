bf165d81314bd02e33637b99aae9d88a3a5bca2b
RefinedCandle49 <valdezvargaskevin@gmail.com>
2025-02-22 17:00:59 -0500

    ProductRepository.java created

---
+++ src/main/java/com/app/restaurantapi/repository/ProductRepository.java
@@ -0,0 +1,13 @@
+package com.app.restaurantapi.repository;
+
+import com.app.restaurantapi.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+
+    @Query("SELECT p FROM Product p WHERE p.status = true")
+    List<Product> findAllActiveProducts();
+}
