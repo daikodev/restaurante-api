f4d4e37ab445c66b52c892b1fbdb6bbca4341ae8
RefinedCandle49 <valdezvargaskevin@gmail.com>
2025-02-22 16:56:32 -0500

    Entity product created

---
+++ src/main/java/com/app/restaurantapi/entity/Product.java
@@ -0,0 +1,31 @@
+package com.app.restaurantapi.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "product")
+public class Product {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "code", length = 10, nullable = false)
+    private String code;
+
+    @Column(name = "description", nullable = false)
+    private String description;
+
+    @Column(name = "unitOfMeasure", length = 10, nullable = false)
+    private String unitOfMeasure;
+
+    @Column(name = "price", nullable = false)
+    private Double price;
+
+    @Column(name = "stock", nullable = false)
+    private Integer stock;
+}
