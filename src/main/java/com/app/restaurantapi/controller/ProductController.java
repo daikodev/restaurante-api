6fe173d6303be46459b49dd7c197de7b17feb2eb
DaikoDev <cdanieleon28@gmail.com>
2025-02-26 01:52:51 -0500

    Ahora el API acepta x-www

--- src/main/java/com/app/restaurantapi/controller/ProductController.java
+++ src/main/java/com/app/restaurantapi/controller/ProductController.java
@@ -1,6 +1,7 @@
 package com.app.restaurantapi.controller;
 
 import com.app.restaurantapi.entity.Product;
+import com.app.restaurantapi.repository.ProductRepository;
 import com.app.restaurantapi.service.ProductService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -13,9 +14,11 @@ import java.util.List;
 public class ProductController {
 
     private final ProductService productService;
+    private final ProductRepository productRepository;
 
-    public ProductController(ProductService productService) {
+    public ProductController(ProductService productService, ProductRepository productRepository) {
         this.productService = productService;
+        this.productRepository = productRepository;
     }
 
     @GetMapping("/all")
@@ -28,14 +31,37 @@ public class ProductController {
         return productService.findProductByCode(code);
     }
 
+//    @PostMapping("/save")
+//    public Product saveProduct(@RequestBody Product product) {
+//        return productService.saveProduct(product);
+//    }
+
     @PostMapping("/save")
-    public Product saveProduct(@RequestBody Product product) {
+    public Product saveProduct(@RequestParam String code, @RequestParam String description, @RequestParam String unitOfMeasure, @RequestParam Double price, @RequestParam Integer stock, @RequestParam Boolean status) {
+
+        Product product = new Product();
+        product.setCode(code);
+        product.setDescription(description);
+        product.setUnitOfMeasure(unitOfMeasure);
+        product.setPrice(price);
+        product.setStock(stock);
+        product.setStatus(status);
+
         return productService.saveProduct(product);
     }
 
     @PatchMapping("/update/{id}")
-    public Product updateProduct(@RequestBody Product product, @PathVariable int id) {
-        return productService.updateProduct(product, id);
+    public Product updateProduct(@PathVariable int id, @RequestParam String code, @RequestParam String description, @RequestParam String unitOfMeasure, @RequestParam Double price, @RequestParam Integer stock, @RequestParam Boolean status) {
+
+        Product product = new Product();
+        product.setCode(code);
+        product.setDescription(description);
+        product.setUnitOfMeasure(unitOfMeasure);
+        product.setPrice(price);
+        product.setStock(stock);
+        product.setStatus(status);
+
+        return productService.updateProduct(id, product);
     }
 
     @PatchMapping("/delete/{id}")
